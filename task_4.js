f746474720bb8dcdd895a10853f4775f010583d8
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-09-19 02:11:00 +0400

    df

--- task_4.js
+++ task_4.js
@@ -5,16 +5,17 @@ const pricePerDroid = 3000;
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const ACCESS_DENIED = 'Недостаточно средств на счету!';
 
-let totalPrice = orderPieces*pricePerDroid; // Write code on this line
-let balanceCredit = credits-totalPrice; // Write code on this line
+let totalPrice = orderPieces * pricePerDroid; // Write code on this line
+let balanceCredit = credits - totalPrice; // Write code on this line
 let message;
 
 // Write code under this line
-if (orderPieces === 4){
-  message = 'Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit}кредитов';}
-else if (orderPieces === null){
-   message = CANCELED_BY_USER;}
-else{
-   message = ACCESS_DENIED;}
-
+if (totalPrice <= credits){
+    message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;}
+ 
+  else{
+     message = ACCESS_DENIED;}
+  
+if (orderPieces === null){
+     message = CANCELED_BY_USER;}
 console.log(message);
